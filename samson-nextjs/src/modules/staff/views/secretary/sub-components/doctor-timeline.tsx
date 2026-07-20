b0cb863b547b11d048d549c7b35894839a12b0cf
ChrstprJohn <ChrstprJohn@users.noreply.github.com>
2026-07-20 12:27:50 +0800

    feat(secretary): move time next to service on card, hide if under 20 mins

--- samson-nextjs/src/modules/staff/views/secretary/sub-components/doctor-timeline.tsx
+++ samson-nextjs/src/modules/staff/views/secretary/sub-components/doctor-timeline.tsx
@@ -330,22 +330,24 @@ export function DoctorTimeline({
 
                       {/* Content Column */}
                       <div className="flex-1 min-w-0 flex flex-col justify-start py-1 px-1 h-full min-h-0 gap-[2px]">
-                        {/* Top Row: Name on Left, Time on Right */}
+                        {/* Top Row: Name on Left */}
                         <div className="flex justify-between items-start gap-2 w-full">
                           <div className={`font-normal truncate text-sm leading-none ${isSelected ? 'font-medium' : ''} ${color.text}`} title={patientName}>
                             {patientName}
                           </div>
-                          {timeRange && !(viewMode === 'week' && doctors.length > 1) && (
-                            <span className={`text-[10px] leading-none shrink-0 pt-0.5 font-normal ${color.subtext}`}>
-                              {timeRange.toLowerCase().replace(/ /g, '')}
-                            </span>
-                          )}
                         </div>
 
-                        {/* Bottom Row: Service */}
-                        <div className={`truncate text-xs leading-none font-normal ${color.subtext}`} title={serviceName}>
-                          {serviceName}
-                        </div>
+                        {/* Bottom Row: Service and Time */}
+                        {!isSmallCard && (
+                          <div className={`truncate text-xs leading-none font-normal ${color.subtext}`} title={`${serviceName}${timeRange && !(viewMode === 'week' && doctors.length > 1) ? ` (${timeRange})` : ''}`}>
+                            {serviceName}
+                            {timeRange && !(viewMode === 'week' && doctors.length > 1) && (
+                              <span className="opacity-80 ml-1.5 font-normal">
+                                ({timeRange.toLowerCase().replace(/ /g, '')})
+                              </span>
+                            )}
+                          </div>
+                        )}
                       </div>
                     </div>
                   );
